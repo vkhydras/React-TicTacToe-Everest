3232d806f7afa7a3a0dec59315655db1885be4b0
vkhydras <victorkimaru8@gmail.com>
2023-06-28 10:55:19 +0300

    appending x and o

--- src/components/TurnIndicator.jsx
+++ src/components/TurnIndicator.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-function TurnIndicator() {
+function TurnIndicator(props) {
+  const styles ={
+    backgroundColor:props.turn &&"#FF2E63"
+  }
+  const styles1 ={
+    backgroundColor:!props.turn &&"#FF2E63"
+  }
   return (
     < div className="turnIndicator">
       <div id="turn">X'S TURN</div>
       <div className="turnContainer">
-        <div id="X" className="turnBox">X</div>
-        <div id="O" className="turnBox">O</div>
+        <div id="X" className="turnBox" style={styles}>X</div>
+        <div id="O" className="turnBox" style={styles1}>O</div>
       </div>
     </div>
   );
