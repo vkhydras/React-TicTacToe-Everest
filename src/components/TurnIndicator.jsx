24f02395d1dbe0d4e252174b9d11e2d050232de9
AlvinKab <133643257+AlvinKab@users.noreply.github.com>
2023-06-29 14:16:04 +0300

    added exit button and modified restart button

--- src/components/TurnIndicator.jsx
+++ src/components/TurnIndicator.jsx
@@ -7,11 +7,14 @@ function TurnIndicator(props) {
   const styles1 ={
     backgroundColor:!props.turn && !props.gameOver &&"#FF2E63"
   }
+  const styleBold = {
+    fontWeight: "bolder"
+  }
 
   return (
     < div className="turnIndicator">
-      {!props.win && !props.draw ?<div id="turn">{props.turn ? "X":"O"}'S TURN</div>
-      :<div id="turn" className={props.win &&"winning-message"}>
+      {!props.win && !props.draw ?<div id="turn" style={styleBold}>{props.turn ? "X":"O"}'S TURN</div>
+      :<div id="turn" className={props.win &&"winning-message"} style={styleBold}>
          {props.win && <span>{props.gameOver ? "😀"+props.winner +" ":""}</span>}
          {props.draw &&!props.win ? "😥 DRAW":"WON"}
          </div>}
